3fc8e7f6c60cb3a31d45fbd33544cfef50d72603
Luana Lima da Silva Martins <luanalima.silva20147@gmail.com>
2026-02-15 07:48:31 -0300

    Criado ex018

--- exercicios/ex018/script.js
+++ exercicios/ex018/script.js
@@ -1,4 +1,26 @@
-let inicio = window.document.getElementById('iinicio')
-let fim = window.document.getElementById('ifim')
-let passo = window.document.getElementById('ipasso')
+document.getElementById('botao').addEventListener('click', function(event) {
+    event.preventDefault()
+    calcular()
+})
 
+function calcular() {
+    let numeroInput = document.getElementById('num').value
+
+    if (numeroInput === '') {
+        alert('[ERRO] Digite um número')
+        return
+    }
+    if (numeroInput == 0) {
+        alert('0 não é um número multiplicável')
+    }
+    
+    let número = Number(numeroInput)
+    let mult = 0
+    let resultado = document.getElementById('resultado')
+
+    for (tabuada = 1; tabuada <= 10; tabuada++) {
+        mult = número * tabuada
+        resultado.select.innerHTML = `${numeroInput} x ${tabuada} =  ${mult}`
+    }
+
+}
