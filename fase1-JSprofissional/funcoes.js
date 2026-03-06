c617115348a5135f6bdeff1c267f806dbe073412
Luana Lima da Silva Martins <luanalima.silva20147@gmail.com>
2026-03-06 16:24:01 -0300

    lista aparecendo na tela

---
+++ fase1-JSprofissional/funcoes.js
@@ -0,0 +1,43 @@
+// FUNÇÕES DECLARADAS
+/*function saudacao() {
+    console.log("Olá!")
+}
+
+saudacao()*/
+
+
+// FUNÇÕES ANÔNIMAS
+/*const saudacao = function() {
+    console.log("Olá")
+}
+
+saudacao()*/
+
+// PARÂMETROS
+/*function saudacao(nome) {
+    console.log("Olá " + nome)
+}
+
+saudacao("Lua")*/
+
+// Exemplo com dois parâmetros
+/*function soma(a, b) {
+    console.log(a + b)
+}
+
+soma(5, 4)*/
+
+// RETURN
+/*function soma(a, b) {
+    return a + b
+}
+
+const resultado = soma(4, 6)
+console.log(resultado)*/
+
+// ARROW FUNCTIONS (=>)
+/*const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))*/
