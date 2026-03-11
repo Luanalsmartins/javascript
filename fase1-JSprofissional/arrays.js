568b692364eca14616685c490b3dbe853f27224e
Luana Lima da Silva Martins <luanalima.silva20147@gmail.com>
2026-03-11 13:59:04 -0300

    revisando arrays

---
+++ fase1-JSprofissional/arrays.js
@@ -0,0 +1,37 @@
+// PERCORRENDO ARRAYS COM FOR 
+/*let frutas = ["maçã", "banana", "laranja"]
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}*/
+
+// PERCORRENDO ARRAYS COM FOR...OF
+/*let frutas = ["maçã", "banana", "laranja"]
+
+for (let fruta of frutas) {
+    console.log(fruta)
+}*/
+
+// includes()
+/*let frutas = ["maçã", "banana", "laranja"]
+
+console.log(frutas.includes("banana"))*/
+
+// indexOf()
+/*let frutas = ["maçã", "banana", "laranja"]
+
+console.log(frutas.indexOf("banana"))*/
+
+// slice()
+/*let numeros = [1, 2, 3, 4, 5]
+
+let parte = numeros.slice(1, 4)
+
+console.log(parte)*/
+
+// splice()
+/*let frutas = ["maçã", "banana", "laranja"]
+
+frutas.splice(1, 1)
+
+console.log(frutas)*/
--- fase1-JSprofissional/funcoes.js
+++ fase1-JSprofissional/funcoes.js
@@ -41,3 +41,60 @@ console.log(resultado)*/
 }
 
 console.log(soma(2, 3))*/
+
+// PARÂMETROS PADRÃO
+/*function saudacao(nome = "visitante") {
+    console.log("Olá " + nome)
+}
+
+saudacao()*/
+
+// REST PARAMETERS (...)
+/*function soma(...numeros) {
+    let total = 0
+
+    for (let n of numeros) {
+        total += n
+    }
+    return total
+}
+
+console.log(soma(2, 3, 4))*/
+
+// CALLBACKS
+/*function executar(funcao) {
+    funcao()
+}
+
+executar(function() {
+    console.log("Executando callback")
+})*/
+/*const numeros = [1, 2, 3]
+
+numeros.forEach(function(n) {
+    console.log(n)
+})*/
+
+// FUNÇÕES DENTRO DE FUNÇÕES
+/*function externa() {
+    function interna() {
+        console.log("Função interna")
+    }
+    interna()
+}
+externa()*/
+
+// CLOSURES
+/*function contador() {
+    let count = 0
+
+    return function() {
+        count++
+        console.log(count)
+    }
+}
+const contar = contador()
+contar()
+contar()
+contar()
+*/
